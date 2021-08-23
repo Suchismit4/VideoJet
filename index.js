a740d8ea75130f72a23d48a1aba3f0d34477932d
Suchismit4 <34796828+Suchismit4@users.noreply.github.com>
2021-08-23 10:50:38 +0530

    1:37:00

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require('express');
+// imports
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
 
-app.get('/', (req, res) => {
-    res.status(200).write('Hello world!');
-})
+// settings
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+app.use('/peerjs', peerServer);
+
+// root route for creating rooms.
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+
+// getting the uuid room route
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+// when a new user connects to our network
+io.on("connection", (socket) => {
+  // when the event 'join-room' is triggered we are to listen to it.
+  socket.on("join-room", (roomId, userId) => {
+    // joining with roomId from front-end (creating a socket room)
+    socket.join(roomId);
+    // telling all others that a new user has joined
+    socket.to(roomId).emit("user-connected", userId);
+  });
+});
 
 server.listen(3030);
