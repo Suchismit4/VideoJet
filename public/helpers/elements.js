2fded721fc57459e62ced92543e60a26af97681b
Suchismit Ghosh <suchismit4@gmail.com>
2021-09-20 02:42:35 +0530

    Toggle video

--- public/helpers/elements.js
+++ public/helpers/elements.js
@@ -4,21 +4,27 @@ const _videoGrid = document.getElementById("video-grid");
 const CreateElement = {
     CreateAudioBlock: (track, stream, muted) => {
         elementCount++;
-        let DOM = `  <div class="audio-block" id="${track.id}">
-                            <div class="pfp"></div>
-                    </div>`
+        // structure: 
+        // <div class="audio-block" id="${track.id}">
+        //                 <div class="audio-block-content">
+        //                     <div class="pfp"></div>
+        //                 </div>
+        //             </div>
         DOM = document.createElement('div');
         DOM.classList.add('audio-block');
         DOM.id = track.id;
+        let audio_content = document.createElement('div');
+        audio_content.classList.add('audio-block-content');
+        DOM.append(audio_content);
         let pfp = document.createElement('div');
         pfp.classList.add('pfp')
-        DOM.append(pfp);
+        audio_content.append(pfp);
         _videoGrid.appendChild(DOM);
         DOM = document.getElementById(track.id);
         var sound = document.createElement('audio');
         sound.autoplay = 'true'
         if(!muted) sound.srcObject      = stream;
-        DOM.appendChild(sound);
+        audio_content.appendChild(sound);
         updateVideos();
         return true;
     }
